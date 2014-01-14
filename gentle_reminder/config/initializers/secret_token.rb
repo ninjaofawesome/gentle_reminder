6dc26f816c7ccd5c587202f1390c8a157b7b11f2
Gustavo <gustavoguimaraes@gmail.com>
2014-01-14 13:28:45 -0500

    Push rails

---
+++ gentle_reminder/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GentleReminder::Application.config.secret_token = '4e1a0035f64e71f927ca669f923c628ded8c2f41391db10dbf26be1a56674836c364850e8722726ac517c9acaa6caa1089eba82afea9a2d42c40c6f7f2e650e3'
