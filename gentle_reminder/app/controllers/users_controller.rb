79e9dbf97e49b8fae549046eff8fd0f03beacb9f
Gustavo <gustavoguimaraes@gmail.com>
2014-01-15 20:14:47 -0500

    Troubleshoot goals controller

--- gentle_reminder/app/controllers/users_controller.rb
+++ gentle_reminder/app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
 
 	def index 
-		# log in page
-  #   If user exists go to show page (render :show)
-  #   Else
-  #    continue to new (render :new)
+  	# log in page
+    #   If user exists go to show page (render :show)
+    #   Else
+    #    continue to new (render :new)
 	end 
 
   def new
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to "/users/#{@user.id}/goals/new"
+      # redirect_to "/users/#{@user.id}/goals/new"
+       redirect_to new_user_goal_path(@user, @goal)
     else
       render "new"
     end
