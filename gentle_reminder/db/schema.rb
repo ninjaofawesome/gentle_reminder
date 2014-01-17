2c9036cb59adaf87cc62a2f72dd3497ad5f348c2
fdw101 <felicia.ogarro@gmail.com>
2014-01-17 15:37:30 -0500

    update schema after seed

--- gentle_reminder/db/schema.rb
+++ gentle_reminder/db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(:version => 20140116231623) do
 
   create_table "programming_languages", :force => true do |t|
     t.string   "name"
-    t.integer  "goal_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
