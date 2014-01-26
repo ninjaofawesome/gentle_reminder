1ac1832b2e911131f7f0cd9f47de995cabdfc6a7
Chris <chris.haack@me.com>
2014-01-26 14:07:29 -0500

    add meetup provider to omniauth.rb in initializers

--- gentle_reminder/config/initializers/omniauth.rb
+++ gentle_reminder/config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
-
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
   provider :meetup, ENV['MEETUP_KEY'], ENV['MEETUP_SECRET']
