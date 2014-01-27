7b3362c55332e0a8f8537e6cc12913ca5cd06e0c
Chris <chris.haack@me.com>
2014-01-27 17:23:44 -0500

    make changes

--- gentle_reminder/app/controllers/sessions_controller.rb
+++ gentle_reminder/app/controllers/sessions_controller.rb
@@ -1,21 +1,25 @@
 class SessionsController < ApplicationController
-
+require 'debugger'
 	def new 
 
 	end 
 
 	def create 
 		#raise env["omniauth.auth"].to_yaml
-		omniuser = User.from_omniauth(env["omniauth.auth"])
-
-		user = User.find_by_email(params[:session][:email].downcase)
-		if user && user.authenticate(params[:session][:password])
-		sign_in user
-		redirect_to user
+		user = User.from_omniauth(env["omniauth.auth"])
+		if user
+			session[:user_id] = user.id
+			redirect_to root_url, notice: "Signed in!"
 		else
-			flash[:error]= "Invalid email or password"
-			render 'new'
-		end 
+		user = User.find_by_email(params[:session][:email].downcase)
+			if user && user.authenticate(params[:session][:password])
+			sign_in user
+			redirect_to user
+			else
+				flash[:error]= "Invalid email or password"
+				render 'new'
+			end
+		end
 	end 
 
 	def destroy
--- gentle_reminder/db/schema.rb
+++ gentle_reminder/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140125212652) do
+ActiveRecord::Schema.define(:version => 20140126233212) do
 
   create_table "charities", :force => true do |t|
     t.string   "name"
@@ -56,6 +56,9 @@ ActiveRecord::Schema.define(:version => 20140125212652) do
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "nickname"
   end
 
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
