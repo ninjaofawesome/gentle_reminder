dbfc055d1dcd40ab9ca1570476eefe62d4a6bc4f
Gustavo <gustavoguimaraes@gmail.com>
2014-01-29 10:44:10 -0500

    Revert "add github key"
    This reverts commit f348826d4be81e9ff9790a4b6bd75f9143ab9e93.

--- gentle_reminder/app/controllers/sessions_controller.rb
+++ gentle_reminder/app/controllers/sessions_controller.rb
@@ -5,20 +5,13 @@ class SessionsController < ApplicationController
 	end 
 
 	def create 
-		#raise env["omniauth.auth"].to_yaml
-		user = user.from_omniauth(env["omniauth.auth"]);
-		if user
-			session[:user_id] = user.user_id
-			redirect_to root_url, notice: 'sign_in'
+		user = User.find_by_email(params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+		sign_in user
+		redirect_to user_goals_path(user)
 		else
-			user = User.find_by_email(params[:session][:email].downcase)
-			if user && user.authenticate(params[:session][:password])
-			sign_in user
-			redirect_to user_goals_path(user)
-			else
-				flash[:error]= "Invalid email or password"
-				render 'new'
-			end
+			flash[:error]= "Invalid email or password"
+			render 'new'
 		end 
 	end 
 
