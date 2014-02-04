03774c4c2aaf6c3fb37836a26385b70f20211b37
Gustavo <gustavoguimaraes@gmail.com>
2014-02-04 09:25:31 -0500

    some fix

--- gentle_reminder/app/models/github.rb
+++ gentle_reminder/app/models/github.rb
@@ -1,12 +1,24 @@
 class Github
 
-  attr_reader :client
+  attr_reader :client, :user
 
   def initialize(user) 
     @client = Octokit::Client.new :access_token => user.github_token
+    @user = user
     @client.api_endpoint = "https://api.github.com" 
   end
 
+  def build_repo_name(repo)
+    user.github_name + "/#{repo}"
+  end
+
+  def commits(repo, branch="master", date)
+    self.client.commits(build_repo_name(repo), branch, {:since => date})
+  end
+
+  def count_commits
+    commits.size
+  end
 
   #other Octokit methods here like get.data
 end
