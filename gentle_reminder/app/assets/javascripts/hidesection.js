563a36f69bde136eaa8bb758ba6aec8ec4de6127
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-02-06 18:06:27 -0500

    made some tweaks to the views

---
+++ gentle_reminder/app/assets/javascripts/hidesection.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	console.log('the page loaded');
+	$('#email').click(function(){
+		// alert('you clicked the button.');
+		$('.sign-in-right-col').removeClass('sign-in-right-col');
+		/*remove the classes emphasis box, right column, needspace, login buttons,etc*/
+
+	})
+
+})
